2e9bb07e1d3ea756800dfe914bf8ecfafbd1e433
Sanchita Shirur <sanchita@Sanchitas-MacBook-Pro.local>
2023-09-24 17:16:36 -0700

    Fixed Camera Angel

--- prototype1/Assets/Scripts/Sphere_Follower.cs
+++ prototype1/Assets/Scripts/Sphere_Follower.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Sphere_Follower : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(15, 10, -20);
 
     // Start is called before the first frame update
     void Start()
